1a99e3ab777650d0b5ad40e19d1439cc107a40ef
walchko <walchko@users.noreply.github.com>
2017-12-28 17:37:33 +0000

    update

--- test.py
+++ test.py
@@ -3,7 +3,23 @@
 from __future__ import division, print_function
 import turtle
 import time
+import scrollphathd
 
+
+class LED(object):
+    def __init__(self):
+        self.r = 17
+        self.c = 7
+        scrollphathd.set_brightness(0.5)
+        self.brightness = 0.5
+
+    def __del__(self):
+        pass
+
+    def draw(self, face, row_offset=0):
+        for i in range(self.r):
+            for j in range(self.c):
+                scrollphathd.pixel(i, j, self.brightness)
 
 class LEDTurtle(turtle.Turtle):
     """
